e48195ec1852477b214c3c2c0cd60e6b84a7a7c6
Sahil Sharma <officialsahil548k@gmail.com>
2026-06-16 00:23:09 +0530

    Home Page creation in progress

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from 'react-router-dom';
 
 // import publishable key from .env file
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -14,6 +15,8 @@ if (!publishableKey) {
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={publishableKey}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </ClerkProvider>,
 );
