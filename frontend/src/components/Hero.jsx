c8e3ed29b6452c18803c15925ba15ef6809c6edc
Sahil Sharma <officialsahil548k@gmail.com>
2026-06-20 12:27:53 +0530

    corrected a typo

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -218,7 +218,7 @@ const Hero = () => {
                             Preview
                         </span>
                     </button>
-                    <button classNmae={heroStyles.sendButton}>
+                    <button className={heroStyles.sendButton}>
                         <span className={heroStyles.sendButtonText}>
                             Send Invoice
                         </span>
