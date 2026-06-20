b1a2b1f2142bfc6eb7268ada7951e7f5b8436b44
Sahil Sharma <officialsahil548k@gmail.com>
2026-06-20 12:28:39 +0530

    created a pricing and footer components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
+import Pricing from '../components/Pricing'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,7 +13,9 @@ const Home = () => {
         <div className="">
           <Features />
         </div>
+        <Pricing />
       </main>
+      <Footer />
     </div>
   )
 }
